31b1f3f7caf5936ed66ba0f737aeae1ce214b4cd
AzurIce <973562770@qq.com>
2024-11-20 17:21:39 +0800

    fix: Support zero length (or single point) beziers to vertex

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,7 +1,8 @@
 pub mod rate_functions;
 
-use bezier_rs::{Identifier, Join, Subpath, SubpathTValue};
+use bezier_rs::{Bezier, Identifier, Join, Subpath, SubpathTValue};
 use glam::{vec3, vec4};
+use log::trace;
 
 use crate::pipeline::simple;
 
@@ -27,11 +28,19 @@ pub enum SubpathWidth {
     Middle(f32),
 }
 
-pub fn sub_path_to_vertex<Id: Identifier>(
-    subpath: Subpath<Id>,
-    width: SubpathWidth,
-) -> Vec<simple::Vertex> {
+pub fn beziers_to_vertex(beziers: Vec<Bezier>, width: SubpathWidth, closed: bool) -> Vec<simple::Vertex> {
+    trace!("converting subpath to vertex: {:?}", beziers.len());
     const MAX_STEPS: usize = 256;
+
+    let beziers = beziers
+        .into_iter()
+        .filter(|bezier| !bezier.is_point())
+        .collect::<Vec<_>>();
+    let subpath: Subpath<Id> = Subpath::from_beziers(&beziers, closed);
+
+    if subpath.len() == 0 {
+        return vec![simple::Vertex::default(); 3];
+    }
 
     // https://github.com/3b1b/manim/blob/master/manimlib/shaders/quadratic_bezier/stroke/geom.glsl
     let (inner_path, outer_path) = match width {
@@ -48,6 +57,7 @@ pub fn sub_path_to_vertex<Id: Identifier>(
             subpath.offset(-w as f64 / 2.0, Join::Bevel),
         ),
     };
+    trace!("inner: {:?}, outer: {:?}", inner_path.len(), outer_path.len());
     let mut vertices = vec![];
     for i in 0..MAX_STEPS {
         let t = i as f64 / (MAX_STEPS - 1) as f64;
