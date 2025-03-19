f5e9ed084f0f7b3564109b12d58d66bae2c9ca10
AzurIce <973562770@qq.com>
2025-03-19 11:40:59 +0800

    feat: implemented Debug for EvalResult, Animation and AnimSchedule

--- examples/test_scene/main.rs
+++ examples/test_scene/main.rs
@@ -30,30 +30,13 @@ impl TimelineConstructor for TestScene {
         timeline: &'t RanimTimeline,
         camera: &'r mut Rabject<'t, CameraFrame>,
     ) {
-        // let svg = SvgItem::from_svg(SVG);
 
-        let svg = SvgItem::from_svg(typst_svg!("R"));
-
-        let mut svg = timeline.insert(svg);
-        let fill = svg.data.fill_color();
-        svg.transform(|data| {
-            data.scale(Vec3::splat(10.0))
-                .set_fill_opacity(0.0)
-                .set_stroke_width(1.0)
-                .set_stroke_color(fill)
-                .set_stroke_opacity(1.0);
-        })
-        .apply();
-        // timeline.play(svg.unwrite().with_duration(2.0));
-        // timeline.play(svg.uncreate().with_duration(2.0));
+        let item = Square(500.0).build();
+        timeline.forward(1.0);
+        timeline.sync();
         timeline.play(camera.transform(|camera| camera.fovy = PI / 4.0));
-        // timeline.play(svg.write().with_duration(2.0));
-        // svg.transform(|svg| {
-        //     svg.scale(Vec3::splat(3.272)).scale(Vec3::splat(2.0));
-        // })
-        // .apply();
 
-        // timeline.forward(10.0);
+        timeline.sync();
     }
 }
 
