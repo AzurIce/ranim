b3399e7c41ba20a296c6f7a3c04d5851adc41494
AzurIce <973562770@qq.com>
2025-05-18 18:42:59 +0800

    update hanoi example

--- examples/hanoi/main.rs
+++ examples/hanoi/main.rs
@@ -1,9 +1,9 @@
 use glam::dvec3;
 use ranim::{
-    animation::transform::TransformAnimSchedule,
+    animation::transform::TransformAnim,
     color::{HueDirection, palettes::manim},
     components::Anchor,
-    items::{group::Group, vitem::Rectangle},
+    items::vitem::geometry::Rectangle,
     prelude::*,
     utils::rate_functions::{ease_in_quad, ease_out_quad, linear},
 };
@@ -28,7 +28,7 @@ fn solve_hanoi(
 struct HanoiScene(pub usize);
 
 impl TimelineConstructor for HanoiScene {
-    fn construct(self, timeline: &RanimTimeline, _camera: &mut PinnedItem<CameraFrame>) {
+    fn construct(self, timeline: &RanimTimeline, _camera: PinnedItem<CameraFrame>) {
         let n = self.0;
         let total_sec = 10.0;
         let rod_width = 0.4;
@@ -36,47 +36,56 @@ impl TimelineConstructor for HanoiScene {
         let rod_section_width = 4.0;
 
         let _rods = timeline.pin([-1, 0, 1].into_iter().map(|i| {
-            let mut rod = Rectangle(rod_width, rod_height).build();
-            rod.set_color(manim::GREY_C).put_anchor_on(
-                Anchor::edge(0, -1, 0),
-                dvec3(i as f64 * rod_section_width, -4.0, 0.0),
-            );
-            rod
+            Rectangle::new(rod_width, rod_height).with(|rect| {
+                rect.fill_rgba = manim::GREY_C;
+                rect.stroke_rgba = manim::GREY_C;
+                rect.put_anchor_on(
+                    Anchor::edge(0, -1, 0),
+                    dvec3(i as f64 * rod_section_width, -4.0, 0.0),
+                );
+            })
         }));
 
         let min_disk_width = rod_width * 1.7;
         let max_disk_width = rod_section_width * 0.8;
         let disk_height = (rod_height * 0.8) / n as f64;
-        let _disks = timeline.pin((0..n).map(|i| {
-            let factor = i as f64 / (n - 1) as f64;
-            let disk_width = min_disk_width + (max_disk_width - min_disk_width) * (1.0 - factor);
-            let mut disk = Rectangle(disk_width, disk_height).build();
-            let color = manim::RED_D.lerp(manim::BLUE_D, factor as f32, HueDirection::Increasing);
-            disk.set_color(color).set_stroke_width(0.0).put_anchor_on(
-                Anchor::edge(0, -1, 0),
-                dvec3(-rod_section_width, -4.0 + disk_height * i as f64, 0.0),
-            );
-            disk
-        }));
+        let _disks = (0..n)
+            .map(|i| {
+                let factor = i as f64 / (n - 1) as f64;
+                let disk_width =
+                    min_disk_width + (max_disk_width - min_disk_width) * (1.0 - factor);
+                timeline.pin(Rectangle::new(disk_width, disk_height).with(|rect| {
+                    let color =
+                        manim::RED_D.lerp(manim::BLUE_D, factor as f32, HueDirection::Increasing);
+                    rect.fill_rgba = color;
+                    rect.stroke_rgba = color;
+                    rect.stroke_width = 0.0;
+                    rect.put_anchor_on(
+                        Anchor::edge(0, -1, 0),
+                        dvec3(-rod_section_width, -4.0 + disk_height * i as f64, 0.0),
+                    );
+                }))
+            })
+            .collect::<Vec<_>>();
 
-        let mut disks = [_disks, Group::new(), Group::new()];
+        let mut disks = [_disks, Vec::new(), Vec::new()];
 
         let anim_duration = total_sec / (2.0f64.powi(n as i32) - 1.0) / 3.0;
         let mut move_disk = |idx_src: usize, idx_dst: usize| {
             let top_disk_y = |idx: usize| disks[idx].len() as f64 * disk_height - 4.0;
             let top_src = top_disk_y(idx_src) - disk_height;
             let top_dst = top_disk_y(idx_dst);
-            let mut disk = disks[idx_src].pop().unwrap();
+            let disk = disks[idx_src].pop().unwrap();
+            let disk = timeline.unpin(disk);
 
-            timeline.play(
+            let disk = timeline.play(
                 disk.transform(|data| {
                     data.shift(dvec3(0.0, 3.0 - top_src, 0.0));
                 })
                 .with_duration(anim_duration)
-                .with_rate_func(ease_in_quad)
-                .apply(),
+                .with_rate_func(ease_in_quad),
             );
-            timeline.play(
+            let disk = timeline.play(
                 disk.transform(|data| {
                     data.shift(dvec3(
                         (idx_dst as f64 - idx_src as f64) * rod_section_width,
@@ -85,19 +94,16 @@ impl TimelineConstructor for HanoiScene {
                     ));
                 })
                 .with_duration(anim_duration)
-                .with_rate_func(linear)
-                .apply(),
+                .with_rate_func(linear),
             );
-            timeline.play(
+            let disk = timeline.play(
                 disk.transform(|data| {
                     data.shift(dvec3(0.0, top_dst - 3.0, 0.0));
                 })
                 .with_duration(anim_duration)
-                .with_rate_func(ease_out_quad)
-                .apply(),
+                .with_rate_func(ease_out_quad),
             );
-            timeline.sync();
-            disks[idx_dst].push(disk);
+            disks[idx_dst].push(timeline.pin(disk));
         };
 
         solve_hanoi(n, 0, 1, 2, &mut move_disk);
