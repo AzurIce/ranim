2c8e7c1acdb1dacb48ce5f03f7ddc0f8f7cbe85a
AzurIce <973562770@qq.com>
2025-09-23 10:15:18 +0800

    update deps and README.md

--- wasm-bindgen-cli.nix
+++ wasm-bindgen-cli.nix
@@ -7,13 +7,13 @@
 buildWasmBindgenCli rec {
   src = fetchCrate {
     pname = "wasm-bindgen-cli";
-    version = "0.2.102";
-    hash = "sha256-onynh0cGDko4RhNHfhvV9xlnyj+H7aPrwcBRIQm24so=";
+    version = "0.2.103";
+    hash = "sha256-ZMK/MpThET2b2uO+9gt9orjXbqLH5ZaoOQ9CAUU9PZY=";
   };
 
   cargoDeps = rustPlatform.fetchCargoVendor {
     inherit src;
     inherit (src) pname version;
-    hash = "sha256-zNK9ihmX2TaHyXY8KuVXHPFGnCp8VouvLiZs7mBvbtM=";
+    hash = "sha256-RYgCb25Cx5x7oLQPkj/2Il6IelvDti1kT+sizEDJETg=";
   };
 }
