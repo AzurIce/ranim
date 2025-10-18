a9f38a5792e3944e59f52aa093c65e4238172f6e
AzurIce <973562770@qq.com>
2025-10-18 11:36:56 +0800

    move main logic into lib

--- packages/ranim-cli/src/lib.rs
+++ packages/ranim-cli/src/lib.rs
@@ -18,7 +18,10 @@ use std::{
 };
 use tracing::{error, info};
 
-use crate::{cli::CliArgs, workspace::Workspace};
+use crate::{
+    cli::{Cli, CliArgs},
+    workspace::Workspace,
+};
 
 pub mod cli;
 pub mod workspace;
@@ -326,4 +329,37 @@ fn get_dylib_path(
     let dylib_name = format!("lib{}.so", artifact_name.replace("-", "_"));
 
     target_dir.join(dylib_name)
+}
+
+/// main
+pub fn main() {
+    use clap::Parser;
+    use tracing::level_filters::LevelFilter;
+    use tracing_subscriber::{EnvFilter, fmt, layer::SubscriberExt, util::SubscriberInitExt};
+
+    fn build_filter() -> EnvFilter {
+        const DEFAULT_DIRECTIVES: &[(&str, LevelFilter)] = &[
+            ("ranim_cli", LevelFilter::INFO),
+            ("ranim", LevelFilter::INFO),
+        ];
+        let mut filter = EnvFilter::from_default_env();
+        let env = std::env::var("RUST_LOG").unwrap_or_default();
+        for (name, level) in DEFAULT_DIRECTIVES
+            .iter()
+            .filter(|(name, _)| !env.contains(name))
+        {
+            filter = filter.add_directive(format!("{name}={level}").parse().unwrap());
+        }
+        filter
+    }
+
+    let indicatif_layer = tracing_indicatif::IndicatifLayer::new();
+
+    tracing_subscriber::registry()
+        .with(fmt::layer().with_writer(indicatif_layer.get_stderr_writer()))
+        .with(indicatif_layer)
+        .with(build_filter())
+        .init();
+
+    Cli::parse().run().unwrap();
 }
